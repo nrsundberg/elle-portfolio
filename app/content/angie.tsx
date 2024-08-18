2dae238767fa499e526c0cac87c5adc997f80f25
nrsundberg <nrsundbe@ncsu.edu>
2024-08-18 18:44:08 -0400

    file correction

--- app/content/angie.tsx
+++ app/content/angie.tsx
@@ -10,7 +10,7 @@ export default function () {
 
       <img
         className="py-2"
-        src={"angele/AngelaHeadleeLogo.PNG"}
+        src={"angela/AngelaHeadleeLogo.PNG"}
         alt={"Angela Headlee final logo"}
       />
 
